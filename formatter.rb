0337f5abf418cc29c83ba80d267949cb8fa3edc8
stevenacz <scoailazaa@gmail.com>
2020-11-20 17:14:45 -0500

    method created card finished

--- formatter.rb
+++ formatter.rb
@@ -47,11 +47,19 @@ module Formatter
 
   def show_list_table_rows(list)
     list.cards.map do |card|
-      members = card.members.join(", ")
-      labels = card.labels.join(", ")
+      members = array_or_string(card.members)
+      labels = array_or_string(card.labels)
       due_date = card.due_date
       [card.id, card.title, members, labels, due_date, n_check(card.checklist)]
     end
+  end
+
+  def array?(value)
+    value.class.to_s == "Array"
+  end
+
+  def array_or_string(value)
+    array?(value) ? value.join(", ") : value
   end
 
   def n_check(checklist)
@@ -60,5 +68,11 @@ module Formatter
       check.completed == true && completed += 1
     end
     "#{completed}/#{checklist.size}"
+  end
+
+  def exit
+    puts "####################################"
+    puts "#   Thanks for using CLIn Boards   #"
+    puts "####################################"
   end
 end
